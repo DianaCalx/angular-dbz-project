80daf87abc471853c7f2a50a8b875eecda5038ef
Diana Calderón <calderon.calx@gmail.com>
2024-02-26 13:31:48 -0600

    First commit

---
+++ src/app/dbz/services/dbz.services.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { v4 as uuid } from 'uuid';
+import { Character } from '../interfaces/character.interface';
+
+@Injectable({ providedIn: 'root' })
+export class DbzService {
+  public characters: Character[] = [
+    { id: uuid(), name: 'Krilin', power: 1000 },
+    { id: uuid(), name: 'Goku', power: 9500 },
+  ];
+
+  addCharacter(character: Character): void {
+    const newCharacter: Character = {
+      ...character,
+      id: uuid(),
+    };
+    this.characters.push(newCharacter);
+  }
+
+  onDeleteCharacterById(id: string): void {
+    //Remove the Character based on id
+    this.characters = this.characters.filter(
+      (character) => character.id !== id
+    );
+  }
+}
